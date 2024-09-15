dc9b4b092ecb737688abef64b11a403613dd84e2
DELL 5400 <DELL 5400@KHLUD>
2024-09-15 17:53:27 +0300

    DAL for Department Module

---
+++ Company444.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Company444.DAL.Data.Configrations;
+using Company444.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company444.DAL.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server = .; Database = MVCCompany444; Trusted_Connection = true;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigrations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+
+    }
+}
---
+++ Company444.DAL/Data/Configrations/DepartmentConfigrations.cs
@@ -0,0 +1,27 @@
+﻿using Company444.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company444.DAL.Data.Configrations
+{
+    internal class DepartmentConfigrations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //fluent api  for "Department" Domain
+
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Code).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(D => D.Name).HasColumnType("varchar").IsRequired();
+
+
+
+        }
+    }
+}
