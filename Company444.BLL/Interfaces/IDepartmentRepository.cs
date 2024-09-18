e16027d8c7d23e13133f9f6fe3a1f3dcdbcdd342
DELL 5400 <DELL 5400@KHLUD>
2024-09-18 18:04:42 +0300

    Generate BLL IDepartmentRepository RepoDepartment

---
+++ Company444.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Company444.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company444.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department Get(int id);
+
+        int Add(Department entity);
+
+        int Update(Department entity);
+
+        int Delete(Department entity);
+    }
+}
---
+++ Company444.BLL/Repositories/RepoDepartment.cs
@@ -0,0 +1,53 @@
+﻿using Company444.BLL.Interfaces;
+using Company444.DAL.Data;
+using Company444.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company444.BLL.Repositories
+{
+    internal class RepoDepartment : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _DbContext;
+
+
+        public RepoDepartment(ApplicationDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public int Add(Department entity)
+        {
+            _DbContext.Add(entity);
+
+            return _DbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _DbContext.Remove(entity);
+
+            return _DbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+            return _DbContext.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        => _DbContext.Departments.AsNoTracking().ToList();
+        
+
+        public int Update(Department entity)
+        {
+            _DbContext.Departments.Update(entity);
+
+            return _DbContext.SaveChanges();
+        }
+    }
+}
